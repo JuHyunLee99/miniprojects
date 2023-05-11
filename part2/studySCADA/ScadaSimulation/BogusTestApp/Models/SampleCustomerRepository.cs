9bfe52e9d550861c4720f5f289da2c76afc57706
JuHyunLee99 <juhyun.lee0829@daum.net>
2023-05-11 12:34:33 +0900

    커밋

--- part2/studySCADA/ScadaSimulation/BogusTestApp/Models/SampleCustomerRepository.cs
+++ part2/studySCADA/ScadaSimulation/BogusTestApp/Models/SampleCustomerRepository.cs
@@ -9,15 +9,15 @@ namespace BogusTestApp.Models
 {
     public class SampleCustomerRepository
     {
-        public IEnumerable<Customer> GetCustomers (int genNum)
+        public IEnumerable<Customer> GetCustomers(int genNum)
         {
-            Randomizer.Seed = new Random(123456);   // Seed갯수를 지정 123456개
-            // 아래와 같은 규칙으로 주문 더미데이터 생성함.
+            Randomizer.Seed = new Random(123456); // Seed갯수를 지정 123456은 여러분마음대로
+            // 아래와 같은 규칙으로 주문 더미데이터슬 생성하겠다
             var orderGen = new Faker<Order>()
-                .RuleFor(o => o.Id, Guid.NewGuid)    // ID값은 GUID로 자동 생성
+                .RuleFor(o => o.Id, Guid.NewGuid)  // ID값은 GUID로 자동 생성
                 .RuleFor(o => o.Date, f => f.Date.Past(3))  // 날짜를 3년전으로 셋팅 생성
-                .RuleFor(o => o.OrderValue, f => f.Finance.Amount(1, 10000))   // 1부터 10000까지 숫자중에서 랜덤하게 셋
-                .RuleFor(o => o.Shipped, f => f.Random.Bool(0.8f));   // 0.5f라면 true/false가 반반
+                .RuleFor(o => o.OrderValue, f => f.Finance.Amount(1, 10000)) // 1부터 10000까지 숫자중에서 랜덤하게 셋
+                .RuleFor(o => o.Shipped, f => f.Random.Bool(0.8f)); // 0.5f라면 true/false가 반반
 
             // 고객더미데이터 생성규칙
             var customerGen = new Faker<Customer>()
@@ -28,7 +28,7 @@ namespace BogusTestApp.Models
                 .RuleFor(c => c.ContactName, f => f.Name.FullName())
                 .RuleFor(c => c.Orders, f => orderGen.Generate(f.Random.Number(1, 2)).ToList());
 
-            return customerGen.Generate(genNum);    // 10개의 가짜 고객데이터를 생성, 리턴
+            return customerGen.Generate(genNum); // n개의 가짜 고객데이터를 생성, 리턴
         }
     }
 }
--- part2/studySCADA/ScadaSimulation/FakeIotDeviceApp/MainWindow.xaml.cs
+++ part2/studySCADA/ScadaSimulation/FakeIotDeviceApp/MainWindow.xaml.cs
@@ -2,24 +2,14 @@
 using FakeIotDeviceApp.Models;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
 using System.Text;
+using System.Diagnostics;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using uPLibrary.Networking.M2Mqtt;
+using System.Windows.Documents;
+using System;
 
 namespace FakeIotDeviceApp
 {
@@ -28,29 +18,30 @@ namespace FakeIotDeviceApp
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
-        Faker<SensorInfo> FackeHomeSwnsor = null;   // 가짜 스마트홈 센서값 변수
-
-        MqttClient client;
+        Faker<SensorInfo> FakeHomeSensor { get; set; } = null; // 가짜 스마트홈 센서값 변수
+        MqttClient Client { get; set; }
         Thread MqttThread { get; set; }
 
+        // MQTT 출력 건수
+        int MaxCount = 50;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            InitFaceData();
+            InitFakeData();
         }
 
-        private void InitFaceData()
+        private void InitFakeData()
         {
             var Rooms = new[] { "Bed", "Bath", "Living", "Dining" };
 
-            FackeHomeSwnsor = new Faker<SensorInfo>()
-                .RuleFor(s => s.Home_Id, "D101H703")  // 임의로 픽스된 홈 아이디
-                .RuleFor(s => s.Room_Name, f => f.PickRandom(Rooms))// 실행할때마다 방이름이 계속 변경
-                .RuleFor(s => s.Sensing_DateTime, f => f.Date.Past(0))    //현재시각이 생성
-                .RuleFor(s => s.Temp, f => f.Random.Float(20.0f, 30.0f))    // 20~30도 사이의 실수 온도값
-                .RuleFor(s => s.Humid, f => f.Random.Float(40.0f, 64.0f));  // 40~64% 사이의 실수 습도값
+            FakeHomeSensor = new Faker<SensorInfo>()
+                .RuleFor(s => s.Home_Id, "D101H703")   // 임의로 픽스된 홈아이디 101동 703호
+                .RuleFor(s => s.Room_Name, f => f.PickRandom(Rooms)) // 실행할때마다 방이름이 계속 변경
+                .RuleFor(s => s.Sensing_DateTime, f => f.Date.Past(0)) // 현재시각이 생성
+                .RuleFor(s => s.Temp, f => f.Random.Float(20.0f, 30.0f)) // 20~30도 사이의 온도값
+                .RuleFor(s => s.Humid, f => f.Random.Float(40.0f, 64.0f)); // 40~64% 사이의 습도값
         }
 
         private async void BtnConnect_Click(object sender, RoutedEventArgs e)
@@ -61,54 +52,66 @@ namespace FakeIotDeviceApp
                 return;
             }
 
-            //브로커아이피로 접속
+            // 브로커아이피로 접속
             ConnectMqttBroker();
-
-            // 하위 로직을 무한 반복
-            StartPublish();
+            // 하위의 로직을 무한반복
+            StartPublish();            
         }
 
+        // 핵심처리 센싱된 데이터값을 MQTT브로커로 전송
         private void StartPublish()
         {
-
-            //센서값  MQTT브로커에 전송
-
-            //RtbLog에 출력
             MqttThread = new Thread(() =>
             {
                 while (true)
                 {
-                    //가짜 스마트홈 센서값 생성
-                    SensorInfo info = FackeHomeSwnsor.Generate();
-                    Debug.WriteLine($"{info.Home_Id} / {info.Room_Name} / {info.Sensing_DateTime} / {info.Temp}");
+                    // 가짜 스마트홈 센서값 생성
+                    SensorInfo info = FakeHomeSensor.Generate();
+                    // 릴리즈(배포)때는 주석처리/삭제
+                    Debug.WriteLine($"{info.Home_Id} / {info.Room_Name} / {info.Sensing_DateTime} / {info.Temp}");                    
+                    // 객체 직렬화 (객체데이터를 xml이나 json등의 문자열로 변환)
+                    var jsonValue = JsonConvert.SerializeObject(info, Formatting.Indented);
                     // 센서값 MQTT브로커에 전송(Publish)
-
-                    // Rtblog 출력
-                
+                    Client.Publish("SmartHome/IoTData/", Encoding.Default.GetBytes(jsonValue));
+                    // 스레드와 UI스레드간 충돌이 안나도록 변경
+                    this.Invoke(new Action(() => {
+                        if(MaxCount <= 0)
+                        {
+                            RtbLog.SelectAll();
+                            RtbLog.Selection.Text = string.Empty;
+                            MaxCount = 50;
+                            RtbLog.AppendText(">>> 문서건수가 많아져서 초기화.\n");
+                        }
+                        // RtbLog에 출력                        
+                        RtbLog.AppendText($"{jsonValue}\n");
+                        RtbLog.ScrollToEnd(); // 스크롤 제일 밑으로 보내기
+                        MaxCount--;
+                    }));
+                    
                     // 1초동안 대기
                     Thread.Sleep(1000);
-
                 }
             });
             MqttThread.Start();
         }
 
+        // 브로커 아이디로 접속
         private void ConnectMqttBroker()
         {
-            client = new MqttClient(TxtMqttBrokerIp.Text);
-            client.Connect("SmartHomeDev"); // publish client ID를 지정
+            Client = new MqttClient(TxtMqttBrokerIp.Text);
+            Client.Connect("SmartHomeDev"); // publish Client ID를 지정
         }
 
         private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            if(client != null && client.IsConnected == true)
+            if (Client != null && Client.IsConnected == true)
             {
-                client.Disconnect();    //  접속을 끊어주고
+                Client.Disconnect(); // 접속을 끊어주고
             }
 
             if (MqttThread != null)
             {
-                MqttThread.Abort(); // 여기가 없으면 프로그램 종료후에도 메모리에 남아있음!!
+                MqttThread.Abort(); // 여기가 없으면프로그램 종료후에도 메모리에 남아있음!!!
             }
         }
     }
